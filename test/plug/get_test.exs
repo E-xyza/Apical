784235ec205f164bb9579a869ded9d1d4c2ca145
Isaac Yonemoto <isaac.yonemoto@gmail.com>
2023-07-10 22:59:23 -0500

    initial test for plug-only deployments

---
+++ test/plug/get_test.exs
@@ -0,0 +1,36 @@
+defmodule ApicalTest.Verbs.GetTest do
+  defmodule Router do
+    require Apical
+
+    Apical.router_from_string(
+      """
+      openapi: 3.1.0
+      info:
+        title: TestGet
+        version: 1.0.0
+      paths:
+        "/":
+          get:
+            operationId: testGet
+            responses:
+              "200":
+                description: OK
+      """,
+      for: Plug,
+      root: "/",
+      controller: ApicalTest.Verbs.GetTest,
+      encoding: "application/yaml"
+    )
+  end
+
+  use ApicalTest.EndpointCase, with: Plug
+  alias Plug.Conn
+
+  def testGet(conn, _params) do
+    Conn.send_resp(conn, 200, "OK")
+  end
+
+  test "GET /", %{conn: conn} do
+    Req.get!("http://localhost:#{@port}/") |> dbg(limit: 25)
+  end
+end
